9506c9896665374ce6241010d4fe61aedac53703
Anas-Altaf <anashafiz926@gmail.com>
2023-05-17 11:04:13 +0500

    Done✔

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <chrono>
+#include <thread>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <Windows.h>
+#include "Header.h"
+#include "menu_header.h"
+using namespace std;
+
+int main()
+{
+    screen();
+	return 0;
+}
